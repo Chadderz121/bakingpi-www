8ecf13e54ca48dc938b646559dc75a26432ff309
Alex Chadwick <Alex.Chadwick@cl.cam.ac.uk>
2015-04-06 16:50:36 +0100

    Initial commit of current live website. I didn't think it was worth the bother of importing the old version history from the original development, but that does exist somewhere! After being uploaded, the course was edited many times in place without version control, which means there are many changes the original history would not cover.

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿function ExpandToggle(topicID) {
+	var expand = document.getElementById(topicID);
+	if (!expand) return true;
+	if (expand.style.display == "none") {
+		expand.style.display = "block"
+	} else {
+		expand.style.display = "none"
+	}
+	return true;
+}
